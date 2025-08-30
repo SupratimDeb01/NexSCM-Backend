c564e422cb142777d5f7fe424eab3642c63e6533
Supratim Deb <supratimdeb04@gmail.com>
2025-08-30 21:24:33 +0530

    Update invoiceController.js

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -176,7 +176,7 @@ const downloadInvoice = async (req, res) => {
     // Generate PDF
        const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath(),
+      headless: "new", // stable headless mode
     });
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: "networkidle0" });
